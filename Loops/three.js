a3aeeb041d166b72e3f11045012e6c596324a0f9
Arshpreet-Kaur23 <165686054+Arshpreet-Kaur23@users.noreply.github.com>
2024-08-03 02:00:48 +0000

    MoreAboutLoops

---
+++ Loops/three.js
@@ -0,0 +1,41 @@
+//for of
+
+// const arr = [1,2,3,4,5,6,7];
+// for(const num of arr){
+//     console.log(num);
+// }
+
+// const greetings = "Hello world!";
+// for(const it of greetings){
+//     console.log(it);
+// }
+
+//Maps - hold unique value pair and it remember insertion order
+// const map = new Map()
+// map.set('IN',"INDIA")
+// map.set('USA',"UNITED STATES OF AMERICA")
+// map.set('FR',"FRANCE")
+// console.log(map);
+
+// for(const [key,value] of map){
+//     console.log(key,':-',value);
+// }
+
+
+//for in  loop 
+
+// const arr = ["wttr",'sttrj',"ferty","sfgtrytj","weytydkgf"];
+// for(const it in arr){
+//     console.log(arr[it]);
+// }
+
+
+//forEach
+
+const coding = ["js","c++","c","python","golang"];
+// coding.forEach( function (val){
+//     console.log(val);
+// })
+coding.forEach((item)=>{
+    console.log(item);
+})
